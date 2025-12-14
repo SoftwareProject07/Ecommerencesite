aded7a7d7b77144375817b294b5420ec09134818
GAUTAM DEV <coolgautamshrivastav@gmail.com>
2025-12-14 15:14:07 +0530

    classothercommittee

--- Ecommerencesite/Model/Classother.cs
+++ Ecommerencesite/Model/Classother.cs
@@ -1,9 +1,9 @@
-﻿namespace Ecommerencesite.Model
-{
-          public class Classother
-          {
-                    public int MyProperty { get; set; }
-                    public string MyProperty1 { get; set; }
-                    public string classdemo { get; set; }   
-          }
-}
+﻿//namespace Ecommerencesite.Model
+//{
+//          public class Classother
+//          {
+//                    public int MyProperty { get; set; }
+//                    public string MyProperty1 { get; set; }
+//                    public string classdemo { get; set; }   
+//          }
+//}

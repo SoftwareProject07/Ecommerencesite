4a0075717c12ee16fc93b0a4adefda62a20b3575
GAUTAM DEV <coolgautamshrivastav@gmail.com>
2026-01-08 13:33:54 +0530

    lates code "--08/01/2026--01:35"

--- Ecommerencesite/MODELDTO/ForgetPasswordUserDto.cs
+++ Ecommerencesite/MODELDTO/ForgetPasswordUserDto.cs
@@ -4,11 +4,11 @@ namespace CRUDAPPLICATION.ModelDTO
 {
           public class ForgetPasswordUserDto
           {
-                    public string Email { get; set; } = string.Empty;
-                    [Required(ErrorMessage = "New Password is required.")]
-                    [StringLength(100, MinimumLength = 6)]
-                    [DataType(DataType.Password)]
-                    public string NewPassword { get; set; } = string.Empty;
+                    public string Email { get; set; } 
+                    //[Required(ErrorMessage = "New Password is required.")]
+                    //[StringLength(100, MinimumLength = 6)]
+                    //[DataType(DataType.Password)]
+                    public string NewPassword { get; set; }
                     
           }
          

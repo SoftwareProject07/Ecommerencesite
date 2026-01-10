bf0ba26b6c4a70d875eaf150927087a093fd2a39
GAUTAM DEV <coolgautamshrivastav@gmail.com>
2026-01-10 10:42:29 +0530

    latest code "api code --10/01/2026--10:44"

--- Ecommerencesite/MODELDTO/UserLogindto.cs
+++ Ecommerencesite/MODELDTO/UserLogindto.cs
@@ -8,6 +8,7 @@ namespace Ecommerencesite.MODELDTO
                     //public string? LastName { get; set; } = null;
                     public string? Email { get; set; } = null;
                     public string? Password { get; set; } = null;
+
                    
                    // [RegularExpression(@"^([0-9]{10})$")]
                     public string? MobileNumber { get; set; } = null;
